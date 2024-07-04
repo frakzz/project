cfdcbfd41c3baab66001ea29849ade897ab00d0f
frakzz <cs@list.ru>
2024-07-04 12:34:57 +0300

    3nd commit

--- 1stfile.php
+++ 1stfile.php
@@ -1,11 +1,14 @@
 <?php
+date_default_timezone_set("Europe/Moscow");
+echo "The time is " . date("h:i:sa") . "\r\n";
+
 $i=rand(1, 2);
 echo "I: ".$i. "\r\n";
 if($i=="1") 
 	{
-	echo "i = 1";
-	} else {
+	echo "i = 1 : ".$i ."\r\n";
+	} else 
 	{
-	echo "i = ".$i;
+	echo "i = 2 : ".$i ."\r\n";
 	}
 ?>
